a29a501ac8fe18b0b9f96f56787c528ff9153fc7
Matthew Blissett <mblissett@gbif.org>
2019-04-12 14:07:03 +0200

    Build to language directories with Apache asis redirect output.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 shopt -s globstar
 
 # Produce the translated adoc source from the po-files.
-for lang in po/??.po; do
+for lang in translations/??.po; do
 	langcode=$(basename $lang .po)
 	for doc in **/*.en.adoc; do
 		po4a-translate -f asciidoc -M utf-8 -m $doc -p $lang -k 0 -l $(dirname $doc)/$(basename $doc .en.adoc).$langcode.adoc
@@ -14,19 +14,32 @@ for lang in po/??.po; do
 done
 
 # Generate the output HTML and PDF.
-for lang in en po/??.po; do
+rm -f **/*.html **/*.pdf *.asis
+for lang in en translations/??.po; do
 	langcode=$(basename $lang .po)
-	asciidoctor     -a lang=$langcode index.$langcode.adoc
-	asciidoctor-pdf -a lang=$langcode index.$langcode.adoc
+	mkdir -p $langcode
+	asciidoctor     -o $langcode/index.$langcode.html -a lang=$langcode index.$langcode.adoc
+	asciidoctor-pdf -o $langcode/index.$langcode.pdf -a lang=$langcode index.$langcode.adoc
+
+	cat > index.$langcode.asis <<EOF
+Status: 303 See Other
+Location: ./$langcode/
+Content-Type: text/html; charset=UTF-8
+Content-Language: $langcode
+Vary: negotiate,accept-language
+
+See <a href="./$langcode/">$langcode</a>.
+EOF
+
 done
 
 # Make translation template
-# po4a-gettextize -f asciidoc -M utf-8 -m index.adoc -p po/index.pot
+# po4a-gettextize -f asciidoc -M utf-8 -m index.adoc -p translations/index.pot
 
 # Update translation
-# po4a-updatepo -f asciidoc -M utf-8 -m index.adoc -p po/da.po
+# po4a-updatepo -f asciidoc -M utf-8 -m index.adoc -p translations/da.po
 
-# po4a-normalize -f asciidoc -M utf-8 po/da.po
+# po4a-normalize -f asciidoc -M utf-8 translations/da.po
 
 # Translate
-# po4a-translate -f asciidoc -M utf-8 -m index.adoc -p po/da.po -k 0 -l index.da.adoc
+# po4a-translate -f asciidoc -M utf-8 -m index.adoc -p translations/da.po -k 0 -l index.da.adoc
